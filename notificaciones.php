5922791856536d75e08f0db52fa94bee4aef0f58
flordemol <54426004+flordemol@users.noreply.github.com>
2020-08-21 15:00:24 -0300

    Update notificaciones.php

--- notificaciones.php
+++ notificaciones.php
@@ -2,6 +2,8 @@
     MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
     MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
 
+    require_once 'vendor/autoload.php';
+
     $merchant_order = null;
 
     switch($_GET["topic"]) {
