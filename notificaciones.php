f880e981c0d84eae36bb53fbfacde311696a4539
Florencia <florenciademollein@gmail.com>
2020-08-21 16:46:25 -0300

    correcciones

--- notificaciones.php
+++ notificaciones.php
@@ -1,7 +1,7 @@
 <?php
    MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
-   MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
 
+   require __DIR__ .  '/vendor/autoload.php';
     $merchant_order = null;
 
     switch($_GET["topic"]) {
